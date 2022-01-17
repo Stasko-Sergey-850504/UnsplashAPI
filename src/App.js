e38c181beea94970037c595696ecd9700905d855
Stasko-Sergey-850504 <dr.kantevrus.15@gmail.com>
2022-01-17 23:40:48 +0300

    JSON LOCAL STORAGE

--- src/App.js
+++ src/App.js
@@ -1,113 +1,129 @@
-import React, {useState,useEffect} from "react";
-import UnsplashImage from "./components/UnsplashImage";
-
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./style/App.css";
 
-
-
+let visibilityBtnMore = "None image";
 
 function App() {
-    const [images, setImage] = useState([]);
-    const [query, setQuery] = useState("");
-    const [page, setPages] = useState(1);
-    const [suggestion, setSuggestion] = useState(false);
+  const [images, setImage] = useState([]);
+  const [query, setQuery] = useState("");
+  const [queries, setQueries] = useState([
+    {
+      createdTime: new Date().toLocaleString(),
+      value: "",
+    },
+  ]);
+  const [page, setPage] = useState(0);
+  const [suggestion, setSuggestion] = useState(false);
 
-    useEffect(() => {
-        fetchImages()
-    }, [])
-    const fetchImages = () => {
-        const apiRoot = "https://api.unsplash.com";
-        const clientID = process.env.REACT_APP_ACCESSKEY;
-        const url = `${apiRoot}/search/photos?page=${page}&query=${query}&client_id=${clientID}`
+  useEffect(() => {
+    setQueries([...JSON.parse(localStorage.getItem("QUERY-HISTORY"))]);
+  }, []);
 
-        axios
-            .get(url)
-            .then(res => {
-                setImage([...res.data.results]);
-            })
-    };
+  useEffect(() => {
+    if (images.length >= 10) {
+      visibilityBtnMore = true;
+    }
+    fetchImages();
+    localStorage.clear();
+    localStorage.setItem("QUERY-HISTORY", JSON.stringify(queries));
+  }, [queries, page]);
 
-    const addToLocalStorage = () => {
-        for (let i = 0; i < localStorage.length; i++) {
-            if (localStorage.getItem(localStorage.key(i)) === query) {
-                localStorage.removeItem(localStorage.key(i));
-            }
+  const dropDown = () => {
+    let dropList = [];
+    const length = queries.length <= 6 ? queries.length : 6;
+
+    for (let i = 1; i < length; i++) {
+      dropList.push(queries[queries.length - i]);
+    }
+    return dropList;
+  };
+
+  const fetchImages = () => {
+    const apiRoot = "https://api.unsplash.com";
+    const clientID = process.env.REACT_APP_ACCESSKEY;
+    //${clientID}
+    const url = `${apiRoot}/search/photos?page=${page}&query=${query}&client_id=${clientID}`;
+
+    axios.get(url).then((res) => {
+      setImage([...images, ...res.data.results]);
+    });
+  };
+
+  const handleClickSugesstionList = (item) => {
+    setQuery(item.value);
+  };
+
+  const handleClickSearchBtn = (event) => {
+    setSuggestion(false);
+    event.preventDefault();
+    setPage(1);
+    setImage([]);
+    setQueries([
+      ...queries.filter((item) => item.value !== query),
+      {
+        createdTime: new Date().toLocaleString(),
+        value: query,
+      },
+    ]);
+  };
+
+  const handleMoreBtn = (event) => {
+    event.preventDefault();
+    setPage(page + 1);
+  };
+
+  if(images.length >= 10) {
+    visibilityBtnMore = <button onClick={handleMoreBtn} className="button">
+      MORE
+    </button>
+  }
+
+  return (
+    <div className="App">
+      <div className="searching-container">
+        <input
+          onClick={() => setSuggestion(true)}
+          onChange={(event) => setQuery(event.target.value)}
+          value={query}
+          type="text"
+          placeholder="Search photos"
+          className="searching-input"
+        />
+        <button onClick={handleClickSearchBtn} type="submit" className="button">
+          Search
+        </button>
+        {
+          <ul>
+            {dropDown().map((item) => {
+              if (item.value !== "" && suggestion === true) {
+                return (
+                  <li
+                    onClick={() => {
+                      handleClickSugesstionList(item);
+                    }}
+                    className="drop-down"
+                  >
+                    {item.value}
+                  </li>
+                );
+              }
+            })}
+          </ul>
         }
-        if(query != "")
-            localStorage.setItem(new Date().toLocaleString(), query.toString());
-    };
+      </div>
 
-    const dropDown = () => {
-        const sortedKeys = Object.keys(localStorage).sort((a, b) => {
-            return localStorage[a] - localStorage[b]
-        });
-        const length = sortedKeys.length;
-        let array = [];
-        for (let i = 1; i < 6; i++) {
-                array.push(localStorage.getItem(sortedKeys[length - i]))
-        }
-        return array;
-    };
+      <div className="wrapper-image">
+        {images.map((image) => (
+          <img src={image.urls.thumb} alt="" className="img-icon"/>
+        ))}
+      </div>
 
-    const handleChange = (event) => {
-        setQuery(event.target.value);
-    };
-
-    const handleClick = (event) => {
-        event.preventDefault();
-        setSuggestion(false);
-        addToLocalStorage();
-        fetchImages();
-    };
-
-    const updateQuery = (value) => {
-        setQuery(value);
-    };
-
-    return (
-        <div className="App">
-
-            <div className="searching-container">
-                <input onClick={() => setSuggestion(true)}
-                       onChange={handleChange}
-                       value={query}
-                       type="text"
-                       placeholder="Search photos"
-                       className="searching-input"
-                />
-                <button onClick={handleClick}
-                        type="submit"
-                        className="button">
-                    Search
-                </button>
-                <ul>
-                    {dropDown().map(value => {
-                        if( value !== null  && suggestion === true) {
-                            return <li onClick={() => {updateQuery(value)
-                                            setSuggestion(false)}}
-                                       className="drop-down">{value}</li>
-                        }
-                    })}
-                </ul>
-            </div>
-
-            <div className="wrapper-image">
-                {images.map(image => (
-                    <UnsplashImage url={image.urls.thumb} key={image.id} />
-                ))}
-            </div>
-
-            <div className="searching-container">
-                <button onClick={(event) => {
-                            setPages(page + 1);
-                            setSuggestion(false);
-                            fetchImages();}}
-                        className="button">MORE
-                </button>
-            </div>
-        </div>
-    );
+      <div className="searching-container">
+        {visibilityBtnMore}
+      </div>
+    </div>
+  );
 }
 
 export default App;
